c24c161c88fb9b341d3ca7fcabd42a541f83c9c4
Jahfer Husain <jahferh@gmail.com>
2015-04-06 22:00:28 -0400

    Set up project.clj for clojars deploy

--- project.clj
+++ project.clj
@@ -5,37 +5,39 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :plugins [[com.cemerick/clojurescript.test "0.3.3"]
-            [com.keminglabs/cljx "0.6.0"]
-            [lein-cljsbuild "1.0.5"]]
-
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3126"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/test.check "0.7.0"]]
 
+  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
   :prep-tasks [["cljx" "once"] "javac" "compile"]
-  :source-paths ["target/generated/src/clj"]
-  :test-paths ["target/generated/test/clj"]
+  :source-paths ["src"]
+  :test-paths ["target/test-classes"]
+
+  :profiles {:dev {:plugins [[com.cemerick/clojurescript.test "0.3.3"]
+                             [com.keminglabs/cljx "0.6.0"]
+                             [org.clojure/clojurescript "0.0-3126"]
+                             [lein-cljsbuild "1.0.5"]]
+                   :aliases {"cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]}}}
+
+  :auto-clean false
 
   :cljx {:builds [{:source-paths ["src"]
-                   :output-path "target/generated/src/clj"
+                   :output-path "target/classes"
                    :rules :clj}
                   {:source-paths ["src"]
-                   :output-path "target/generated/src/cljs"
+                   :output-path "target/classes"
                    :rules :cljs}
                   {:source-paths ["test"]
-                   :output-path "target/generated/test/clj"
+                   :output-path "target/test-classes"
                    :rules :clj}
                   {:source-paths ["test"]
-                   :output-path "target/generated/test/cljs"
+                   :output-path "target/test-classes"
                    :rules :cljs}]}
 
-   :cljsbuild {:builds {:test {:source-paths ["target/generated/src/cljs" "target/generated/test/cljs"]
+   :cljsbuild {:builds {:test {:source-paths ["target/classes" "target/test-classes"]
                               :compiler {:output-to "target/cljs/main.js"
-                                         :optimizations :simple
-                                         :source-map "target/cljs/main.js.map"}}}
+                                         :optimizations :advanced
+                                         :pretty-print true}}}
 
-               :test-commands {"unit" ["slimerjs" :runner "target/cljs/main.js"]}}
-
-   :aliases {"cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]})
+               :test-commands {"unit" ["slimerjs" :runner "target/cljs/main.js"]}})
